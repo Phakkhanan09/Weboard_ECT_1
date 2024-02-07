c4be7dd22e9b90dcd3fd284aa920e446f2f3a4af
Phakkhanan09 <Phakkhanan.nak@gmail.com>
2024-02-07 17:00:52 +0700

    yes

--- index.php
+++ index.php
@@ -6,43 +6,66 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webboard sin</title>
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <title style="text-align: center; font-size: 54px;">WebBoard KakKak</title>
 </head>
 <body>
-    <h1 style="text-align: center;">webboard sinxia</h1>
-    <hr>
+    <div class="container-lg">
+    <h1 style="text-align: center;font-size: 54px;"class="mt-3">WebBoard ECT</h1>
+    <nav class="navbar navbar-expand-lg" style="background-color:#d3d3d3;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+      <ul class="navbar-nav">
+        <?php if(!isset($_SESSION['id'])) { ?>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+        </li>
+        <?php }else{ ?>
+            <li class="nav-item dropdown">
+            <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-circle"></i> <?php echo $_SESSION['username']; ?>
+            </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+          </ul>
+            </li>
+        <?php } ?>
+      </ul>
+    </div>
+</nav>
+<br>
     <form>
-        หมวดหมู่: 
-        <select >
+        หมวดหมู่ :
+        <select>
             <option value="all">--ทั้งหมด--</option>
-            <option value="general">เรื่องทั่วไป</option>
+            <option value="general">เรื่องทั้งไป</option>
             <option value="study">เรื่องเรียน</option>
         </select>
         <?php
-        if (!isset($_SESSION['id'])){
-           echo "<a href=login.php style='float: right';>เข้าสู่ระบบ</a>";
-        }else{
-            echo "<div style='float:right'>
-                    ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
-                    <a href=logout.php> ออกจากระบบ </a>
+            if (!isset($_SESSION['id'])){
+        echo "<a href=login.php style='float: right';>เข้าสู่ระบบ</a>";
+            }else{
+        echo "<div style='float:right'>
+                ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
+                <a href=logout.php>ออกจากระบบ</a>
             </div><br>
-            <a href=newpost.php>สร้างกระทู้ใหม่</a>";
-        }
+                <a href=newpost.php><br>สร้างกระทู้ใหม่</a>";
+            }
         ?>
-        
     </form>
     <ul>
         <?php
         for($i=1;$i<=10;$i++){
-            echo "<li><a href=post.php?id=$i>กระทู้ที่ $i</a>";
-            if(isset( $_SESSION['id']) && $_SESSION['role']=='a'){
-                echo "&nbsp;&nbsp<a href=delete.php?id=$i>ลบ</a>";
+            echo "<li><a href = post.php?id=$i>กระทู้ $i</a>";
+            if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
+                echo "&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
             }
-            
-           echo "</li>";
+            echo "</li>";
         }
         ?>
-        
     </ul>
+    </div>
 </body>
 </html>
