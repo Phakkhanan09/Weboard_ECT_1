b5f9bd17ed5234ddce31124a1079121860801998
Phakkhanan09 <Phakkhanan.nak@gmail.com>
2024-02-28 17:37:04 +0700

    work8 and work9
    work8 and work9

--- Verify.php
+++ Verify.php
@@ -5,12 +5,35 @@ if(isset($_SESSION['id'])){
         die();
 
 }
+$login=$_POST['login'];
+$pwd=$_POST['pwd'];
+$conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+$sql="SELECT * FROM user where login='$login' and password=sha1('$pwd')"; 
+$result=$conn->query($sql);
+if($result->rowCount()==1){
+    $data=$result->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['username']=$data['login'];
+    $_SESSION['role']=$data['role'];
+    $_SESSION['user_id']=$data['id'];
+    $_SESSION['id']=session_id();
+    header("location:index.php");
+        die();
+}else{
+    $_SESSION['error']="error";
+    header("location:index.php");
+        die();
+}
+$conn=null;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Verify<form</title>
 </head>
 <body>
--- register.php
+++ register.php
@@ -11,29 +11,104 @@ if(isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>register</title>
 </head>
 <body>
+    <div class = "container-lg">
+        <h1 style="text-align: center;" class="mt-3"> Webboard sinxia </h1>
+        <?php include "nav.php" ?>
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+                <?php 
+                    if(isset($_SESSION['add_login'])){
+                        if($_SESSION['add_login']=="error"){
+                            echo "<div class=' alert alert-danger'>
+                            ชื่อบัญชีซ้ำหรือฐานข้อมูลมีปัญหา</div>";
+                        }else{
+                            echo "<div class='alert alert-success'>
+                            เพิ่มบัญชีเรียบร้อยแล้ว</div>";
+                        }
+                        unset($_SESSION['add_login']);
+                    }
+                ?>
+                <div class="card border-primary">
+                    <div class="card-header bg-primary text-white">เข้าสู่ระบบ</div>
+                    <div class="card-body">
+                        <form action="register_save.php" method="post">
+                            <div class="row">
+                                <label for="user"class="col-lg-3 col-form-lable">ชื่อบัญชี:</label>
+                                <div class="col-lg-9">
+                                    <input id="user"type="text" name="login" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label for="pwd" class="col-lg-3 col-form-label">รหัสผ่าน:</label>
+                                <div class="col-lg-9">
+                                    <input  id="pwd" type="password" name="pwd" class="form-control" required>
+                                </div>
+
+                            </div>
+                            <div class="row mt-3">
+                                <label for="name"class="col-lg-3 col-form-label">ชื่อ-นามสกุล:</label>
+                                <div class="col-lg-9">
+                                    <input id="name"type="text" name="name" class="form-control" required>
+                                </div>
+
+                            </div>
+                            <div class="row m-3">
+                                <label  class="col-lg-3 form-label">เพศ:</label>
+                                <div class="col-lg-9">
+                                    <div class="form-check">
+                                        <input id="m" type="radio" name="gender" value="m" class="form-check-input" required>
+                                        <label for="m" class="form-check-label">ชาย</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input id="f" type="radio" name="gender" value="f" class="form-check-input" required>
+                                        <label for="f" class="form-check-label">หญิง</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input id="o" type="radio" name="gender" value="o" class="form-check-input" required>
+                                        <label for="o" class="form-check-label">อื่นๆ</label>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                            <div class="row mt-3">
+                                <label for="email" class="col-lg-3 col-form-label">อีเมล:</label>
+                                <div  class="col-lg-9">
+                                    <input id="email" type="email" name="email" class="form-control" required>
+                                </div>
+
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-lg-12 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-primary btn-sm me-2">
+                                        <i class="bi bi-save"></i> สมัครสมาชิก</button>
+                                    <button type=" reset" class="btn btn-danger btn-sm">
+                                        <i class="bi bi-x-square"></i> ยกเลิก</button>
+
+                                </div>
+
+                            </div>
+
+                        </form>
+                        
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+
+        </div>
+    </div>
+        <br>    
     
-            
-    <hr>
-    <form>
-        <table style="border: 2px solid black; width: 30%;" align="center"> 
-            <h1 style="text-align: center;">สมัครสมาชิก</h1><tr><td colspan="2" style="background-color: #6cd2fe;">กรอกข้อมูล</td></tr>
-            <tr><td>ชื่อบัญชี:</td><td><input type="text" name="account" size="30" required></td></tr>
-            <tr><td>รหัสผ่าน:</td><td><input type="password" name="pwd" size="30" required></td></tr>
-            <tr><td>ชื่อ-นามสกุล:</td><td><input type="text" name="name" size="30" required></td></tr>
-            <tr><td>เพศ</td><td>
-                    <input type="radio" name="gender" value="m"> ชาย <br>
-                    <input type="radio" name="gender" value="fm"> หญิง <br>
-                    <input type="radio" name="gender" value="a"> อื่นๆ
-            </td></tr>
-            <tr><td>อีเมล:</td><td><input type="email" name="account" size="30" required></td></tr>
-            <tr><td colspan= "2" align="center"><input type="submit" value="สมัครสมาชิก" ></td></tr>
-        </table>
-    </form>
-    <br>
-    <div style="text-align: center;"><a href="index.html">กลับไปหน้าหลัก</a></div> 
     
 </body>
 </html>
