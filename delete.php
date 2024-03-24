73581b0e998d0347a6b743fa91182f838f0974e4
PHAKKHANAN NAKKOED <Phakkhanan.nak@gmail.com>
2024-03-24 14:54:13 +0700

    งานสุดท้าย
    งานสุดท้าย

--- delete.php
+++ delete.php
@@ -1,11 +1,29 @@
 <?php
 session_start();
+<<<<<<< HEAD
+if(isset ($_SESSION['id']) && $_SESSION['role']=='a'){
+if(isset ($_SESSION['id'])){
+    $id = $_GET['id'];
+    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    $sql = "DELETE FROM post WHERE id = $id";
+=======
 if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
     $id=$_GET['id'];
-    echo "ลบกระทู้ หมายเลข $id";
+    $conn=new PDO("mysql:host=localhost;dbname=webboard;
+    charset=utf8","root","");
+    $sql="DELETE FROM post WHERE id=$id";
+    $conn->exec($sql);
+    $sql="DELETE FROM comment WHERE post_id=$id";
+    $conn->exec($sql);
+    $conn=NULL;
+    header("location:index.php");
+    die();
+    
+   
 }else{
     header("location:index.php");
     die();
 }
 
 ?>
+>>>>>>> 445714bc707019aa06fd31ca8f4768d5b91f3f05
--- newpost.php
+++ newpost.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-if(!isset($_SESSION['id'])){
-        header("location:index.php");
-        die();
-
+if(!isset($_SESSION['id'])) {
+    header("location:index.php");
+    die();
 }
 ?>
 <!DOCTYPE html>
@@ -14,64 +13,60 @@ if(!isset($_SESSION['id'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Webboard Sinxia</title>
+    <title>Document</title>
 </head>
 <body>
-    <div class="container-lg">
-        <h1 style="text-align: center;" class="mt-3">webboard sinxia</h1>
+    <div class="container">
+        <h1 style="text-align: center;" class="mt-3">Webboard Sinxia</h1>
         <?php include "nav.php" ?>
         <div class="row mt-4">
             <div class="col-lg-3 col-md-2 col-sm-1"></div>
             <div class="col-lg-6 col-md-8 col-sm-10">
-                <div class="card-header bg-info text-white">ตั้งกระทู้ใหม่</div>
-                <div class="card-body">
-                    <form action="newpost_save.php" method="post">
-                        <div class="row">
-                            <label class="col-lg-3 col-form-label">หมวดหมู่:</label>
-                            <div class="col-lg-9">
-                                <select name="category" class="form-select">      
-                                <?php 
-                                    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-                                    $sql="SELECT * FROM category";
-                                    foreach($conn->query($sql) as $row){
-                                        echo "<option value=$row[id]>$row[name]</option>";
-                                    }
-                                    $conn=null;
-                                ?>
-                                </select>
+                <div class="card border-info">
+                    <div class="card-header bg-info text-white">ตั้งกระทู้ใหม่</div>
+                    <div class="card-body">
+                        <form action="newpost_save.php" method="post">
+                            <div class="row">
+                                <label class="col-lh-3 col-form-label">หมวดหมู่ : </label>
+                                <div class="col-lg-9">
+                                    <select name="category" class="form-select">
+                                        <?php
+                                            $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+                                            $sql = "SELECT * FROM category";
+                                            foreach($conn->query($sql) as $row) {
+                                                echo "<option value = $row[id]>$row[name]</option>>";
+                                            }
+                                            $conn = null;
+                                        ?>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row mt-3">
-                            <label  class="col-lg-3 col-form-label">หัวข้อ:</label>
-                            <div class="col-lg-9">
-                                <input type="text" name="topic" class="form-control" required>
+                            <div class="row mt-3">
+                                <label class="col-lg-3 col-form-label">หัวข้อ : </label>
+                                <div class="col-lg-9">
+                                    <input type="text" name="topic" class="form-control" require>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row mt-3">
-                            <label class="col-lg-3 col-form-label">เนื้อหา:</label>
-                            <div class="col-lg-9">
-                                <textarea name="comment" row="8" class="form-control" required></textarea>
+                            <div class="row mt-3">
+                                <label class="col-lg-3 col-form-label">เนื้อหา : </label>
+                                <div class="col-lg-9">
+                                    <textarea name="comment" rows="8" class="form-control" require></textarea>
+                                </div>
                             </div>
-
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-lg-12 d-flex justify-content-center">
-                                    <button type="submit" class="btn btn-primary btn-sm me-2">
-                                        <i class="bi bi-save"></i> บันทึกข้อความ</button>
-                                    <button type=" reset" class="btn btn-danger btn-sm">
+                            <div class="row mt-3">
+                                <div class="col-lg-12 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-info btn-sm text-white me-2">
+                                        <i class="bi bi-caret-right-square"></i> บันทึกข้อความ</button>
+                                    <button type="reset" class="btn btn-danger btn-sm">
                                         <i class="bi bi-x-square"></i> ยกเลิก</button>
+                                </div>
                             </div>
-
-                        </div>
-                    </form>
-
+                        </form>
+                    </div>
                 </div>
-
             </div>
-
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
         </div>
-        <div class="col-lg-3 col-md-2 col-sm-1"></div>
-
     </div>
 </body>
 </html>
